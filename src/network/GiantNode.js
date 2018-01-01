0809886528ecd127c161e7c3aa8c219e7732e723
Alexey Bokarev <alexeybokarev@gmail.com>
2018-01-01 09:44:44 +0800

    fix fx code

--- src/network/GiantNode.js
+++ src/network/GiantNode.js
@@ -46,16 +46,12 @@ export default class GiantNode extends EventEmitter {
         return this._client.deployContract(options)
     }
 
-    mountModule(contractAddress, cb) {
-        const m = require('module')
-        const moduleName = `GMD_${contractAddress}`
-
-        //get last created contract code (temp)
-        let contractPath = './build/contracts/', contract, contractRunTime,
-            contractData,
-            contractRunTimeData,
-            newContractRunTimeData,
-            pfeDesc = '\nfunction pfe(pfeVars){console.log(pfeVars)}',
+    getLastFxContract(cb) {
+        /**
+         * get code of last contract from fx
+         * //TODO : create method get code from block tip
+         */
+        let contractPath = './build/contracts/',
             getLatestFile = () => {
                 let latest;
                 const files = fs.readdirSync(contractPath);
@@ -74,29 +70,28 @@ export default class GiantNode extends EventEmitter {
                 });
                 return latest.filename;
             }
-
         let latestContract = getLatestFile()
         let runTimeName = latestContract.slice(0, -3) + 'RunTime.js'
 
-        fs.readFile(contractPath + runTimeName, {encoding: 'utf-8'}, function(err,data){
+        fs.readFile(contractPath + runTimeName, {encoding: 'utf-8'}, function(err,code){
             if (!err) {
-                console.log('received data' );
-                console.log(data)
-                const code = data
-                var res = require('vm').runInThisContext(m.wrap(code))(exports, require, module, __filename, __dirname)
-                logger.info(`Mount module ${moduleName}`)
-                cb(module.exports)
-
+                cb(code)
             } else {
                 console.log(err);
             }
         });
+    }
 
-
+    mountModule(contractAddress, cb) {
+        const m = require('module')
+        const moduleName = `GMD_${contractAddress}`
         //TODO: get code from chain
-        const code = '"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _createClass=function(){function o(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}return function(e,t,n){return t&&o(e.prototype,t),n&&o(e,n),e}}(),_GiantContract=require("../../dist/compile/GiantContract"),_GiantContract2=_interopRequireDefault(_GiantContract);function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}function _possibleConstructorReturn(e,t){if(!e)throw new ReferenceError("this hasn\'t been initialised - super() hasn\'t been called");return!t||"object"!=typeof t&&"function"!=typeof t?e:t}function _inherits(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function, not "+typeof t);e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}),t&&(Object.setPrototypeOf?Object.setPrototypeOf(e,t):e.__proto__=t)}var pfeVars={Program:{count:1,fee:10},StringLiteral:{count:11,fee:4},ExpressionStatement:{count:14,fee:4},Identifier:{count:138,fee:4},MemberExpression:{count:24,fee:4},ObjectProperty:{count:11,fee:4},ObjectExpression:{count:6,fee:4},CallExpression:{count:16,fee:4},VariableDeclarator:{count:7,fee:4},VariableDeclaration:{count:7,fee:4},BinaryExpression:{count:8,fee:4},UpdateExpression:{count:1,fee:4},LogicalExpression:{count:7,fee:4},AssignmentExpression:{count:7,fee:4},IfStatement:{count:7,fee:4},BlockStatement:{count:15,fee:4},ForStatement:{count:1,fee:20},FunctionDeclaration:{count:6,fee:4},ReturnStatement:{count:8,fee:4},FunctionExpression:{count:5,fee:4},ConditionalExpression:{count:3,fee:4},UnaryExpression:{count:6,fee:4},NewExpression:{count:3,fee:4},ThrowStatement:{count:3,fee:4},ThisExpression:{count:4,fee:4},SequenceExpression:{count:0,fee:4},ArrayExpression:{count:2,fee:4},ForInStatement:{count:0,fee:20},WhileStatement:{count:0,fee:20},DoWhileStatement:{count:0,fee:20},WhitePaper:{count:void 0,fee:void 0},getBalance:{count:1,fee:20},address:{count:0,fee:20},buyCoin:{count:0,fee:20},sendCoin:{count:0,fee:20}},Ddd=function(e){function t(){_classCallCheck(this,t);var e=_possibleConstructorReturn(this,(t.__proto__||Object.getPrototypeOf(t)).call(this));return e.balances=[],e}return _inherits(t,_GiantContract2.default),_createClass(t,[{key:"getBalance",value:function(){return this.balances}},{key:"getPfe",value:function(){return pfeVars}}]),t}();function pfe(e){Ddd.pfeVars=e,console.log(e)}exports.default=Ddd;'
-
-
+        this.getLastFxContract((code)=>{
+            console.log(code)
+            var res = require('vm').runInThisContext(m.wrap(code))(exports, require, module, __filename, __dirname)
+            logger.info(`Mount module ${moduleName}`)
+            cb(module.exports)
+        })
     }
 
     initContract(contractAddress) {
@@ -118,9 +113,7 @@ export default class GiantNode extends EventEmitter {
                 for (let i in pfeVars) {
                     console.log(i + ' ' + pfeVars[i])
                 }
-
                 logger.info(`Call method getPfe: ${global.contracts[meta.className].getPfe()}`)
-
             })
         })
     }
