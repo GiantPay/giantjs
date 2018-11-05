c7fe12db0eb2b24a47e4f9cb338524f698a60ded
Alexey Bokarev <alexeybokarev@gmail.com>
2018-11-05 14:45:54 +0800

    fix plug fee

--- src/babel/babel-plugin-contract-fee/index.js
+++ src/babel/babel-plugin-contract-fee/index.js
@@ -1,71 +1,299 @@
 import logger from '../../logger'
-import fs from 'fs'
-
 
 /**
- * @returns write file PFE function of the giant contract code
+ * @returns ast and pfe functions of the giant contract code
  */
-export default () => {
-    const feePrice = 0.00000001
-    var pfeCount = (price, declaration) => {
-        let declarationPrice = price * feePrice
-        return '\tcontractAmount += ' + declarationPrice +
-            '\n\tconsole.log(\'Found ' + declaration +
-            '. Price : ' + declarationPrice + '\')\n\tconsole.log(contractAmount.toFixed(8))\n'
+export default ({types: t}) => {
+    let found_ExportDefaultDeclaration = false
+    let found_ClassDeclaration = false
+
+    let pfeFunction = {
+        "body": [
+            {
+                "type": "FunctionDeclaration",
+                "id": {
+                    "type": "Identifier",
+                    "name": "pfe"
+                },
+                "params": [
+                    {
+                        "type": "Identifier",
+                        "name": "contractName"
+                    },
+                    {
+                        "type": "Identifier",
+                        "name": "declaration"
+                    },
+                    {
+                        "type": "Identifier",
+                        "name": "mockClient"
+                    }
+                ],
+                "body": {
+                    "type": "BlockStatement",
+                    "body": [
+                        {
+                            "type": "VariableDeclaration",
+                            "declarations": [
+                                {
+                                    "type": "VariableDeclarator",
+                                    "id": {
+                                        "type": "Identifier",
+                                        "name": "spendFee"
+                                    },
+                                    "init": {
+                                        "type": "ObjectExpression",
+                                        "properties": [
+                                            {
+                                                "type": "Property",
+                                                "key": {
+                                                    "type": "Identifier",
+                                                    "name": "ClassDeclaration"
+                                                },
+                                                "value": {
+                                                    "type": "Literal",
+                                                    "value": 10,
+                                                    "raw": "10"
+                                                },
+                                                "kind": "init"
+                                            },
+                                            {
+                                                "type": "Property",
+                                                "key": {
+                                                    "type": "Identifier",
+                                                    "name": "ClassMethod"
+                                                },
+                                                "value": {
+                                                    "type": "Literal",
+                                                    "value": 8,
+                                                    "raw": "8"
+                                                },
+                                                "kind": "init"
+                                            },
+                                            {
+                                                "type": "Property",
+                                                "key": {
+                                                    "type": "Identifier",
+                                                    "name": "FunctionDeclaration"
+                                                },
+                                                "value": {
+                                                    "type": "Literal",
+                                                    "value": 8,
+                                                    "raw": "8"
+                                                },
+                                                "kind": "init"
+                                            }
+                                        ]
+                                    }
+                                }
+                            ],
+                            "kind": "const"
+                        },
+                        {
+                            "type": "VariableDeclaration",
+                            "declarations": [
+                                {
+                                    "type": "VariableDeclarator",
+                                    "id": {
+                                        "type": "Identifier",
+                                        "name": "arrayOfRequests"
+                                    },
+                                    "init": {
+                                        "type": "ArrayExpression",
+                                        "elements": []
+                                    }
+                                }
+                            ],
+                            "kind": "let"
+                        },
+                        {
+                            "type": "ForStatement",
+                            "init": {
+                                "type": "VariableDeclaration",
+                                "declarations": [
+                                    {
+                                        "type": "VariableDeclarator",
+                                        "id": {
+                                            "type": "Identifier",
+                                            "name": "i"
+                                        },
+                                        "init": {
+                                            "type": "Literal",
+                                            "value": 0,
+                                            "raw": "0"
+                                        }
+                                    }
+                                ],
+                                "kind": "let"
+                            },
+                            "test": {
+                                "type": "BinaryExpression",
+                                "left": {
+                                    "type": "Identifier",
+                                    "name": "i"
+                                },
+                                "operator": "<",
+                                "right": {
+                                    "type": "MemberExpression",
+                                    "object": {
+                                        "type": "Identifier",
+                                        "name": "mockClient"
+                                    },
+                                    "property": {
+                                        "type": "Identifier",
+                                        "name": "length"
+                                    },
+                                }
+                            },
+                            "update": {
+                                "type": "UpdateExpression",
+                                "operator": "++",
+                                "prefix": false,
+                                "argument": {
+                                    "type": "Identifier",
+                                    "name": "i"
+                                }
+                            },
+                            "body": {
+                                "type": "BlockStatement",
+                                "body": [
+                                    {
+                                        "type": "ExpressionStatement",
+                                        "expression": {
+                                            "type": "CallExpression",
+                                            "callee": {
+                                                "type": "MemberExpression",
+                                                "object": {
+                                                    "type": "Identifier",
+                                                    "name": "arrayOfRequests"
+                                                },
+                                                "property": {
+                                                    "type": "Identifier",
+                                                    "name": "push"
+                                                },
+                                            },
+                                            "arguments": [
+                                                {
+                                                    "type": "CallExpression",
+                                                    "callee": {
+                                                        "type": "Identifier",
+                                                        "name": "checkBalance"
+                                                    },
+                                                    "arguments": [
+                                                        {
+                                                            "type": "MemberExpression",
+                                                            "object": {
+                                                                "type": "Identifier",
+                                                                "name": "mockClient"
+                                                            },
+                                                            "property": {
+                                                                "type": "Identifier",
+                                                                "name": "i"
+                                                            }
+                                                        },
+                                                        {
+                                                            "type": "MemberExpression",
+                                                            "object": {
+                                                                "type": "Identifier",
+                                                                "name": "spendFee"
+                                                            },
+                                                            "property": {
+                                                                "type": "Identifier",
+                                                                "name": "declaration"
+                                                            }
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     }
-    var pfeFuncStr = ''
+
+    let pfeCallFunction = {
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "expression": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "pfe"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Identifier",
+                            "name": "contractName"
+                        },
+                        {
+                            "type": "Identifier",
+                            "name": "declaration"
+                        },
+                        {
+                            "type": "Identifier",
+                            "name": "mockClient"
+                        }
+                    ]
+                }
+            }
+        ]
+    }
 
     return {
-        pre(state) {
-            pfeFuncStr += 'exports.Pfe = function() {\n\tvar contractAmount = 0\n'
-        },
         visitor: {
             Program: (path) => {
-                let foundClassDeclaration = false
-
                 path.traverse({
-                    ClassDeclaration: (subPath) => {
-                        foundClassDeclaration = true
+                    ExportDefaultDeclaration: (subPath) => {
+                        logger.debug('node type : ' + subPath.get('type').node)
+                        found_ExportDefaultDeclaration = true
+                        subPath.insertAfter(pfeFunction)
                         subPath.stop()
                     }
                 })
             },
             ClassDeclaration: (path) => {
-                let foundConstructor = false
-
+                logger.debug('node type : ' + path.get('type').node)
+                found_ClassDeclaration = true
                 path.traverse({
                     ClassMethod(subPath) {
-                        if (subPath.get('kind').node == 'constructor') {
-                            foundConstructor = true
-                            logger.info('found Constructor')
-                            pfeFuncStr += pfeCount(20, subPath.get('kind').node)
-                        }
+                        logger.debug('node type : ' + subPath.get('type').node)
+                        let node = subPath.get('kind').node
+                        logger.debug('node type : ' + node)
+                        subPath.insertAfter(pfeCallFunction)
                         subPath.stop()
                     }
                 })
+            },
+            ClassMethod: (path) => {
+                path.traverse({
+                    //logger.debug('node type : ' + path.get('type').node)
+                })
 
-                if (!foundConstructor) {
-                    throw path.buildCodeFrameError('Constructor not found')
-                }
+            },
+            FunctionDeclaration: (subPath) => {
+                subPath.insertAfter(pfeCallFunction)
+
             },
             CallExpression: (path) => {
                 if (path.isCallExpression()) {
-                    logger.info('CallExpression ' + path.get('callee').get('type').node)
-                    pfeFuncStr += pfeCount(30, path.get('callee').get('type').node)
+                    logger.debug('CallExpression callee type : ' + path.get('type').node)
                 }
             }
         }, post(state) {
-            pfeFuncStr += '\n\treturn contractAmount\n}'
-            fs.writeFile(
-                './build/contracts/' + state.opts.basename + 'Pfe.js',
-                pfeFuncStr,
-                (err) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    logger.info('Function Pfe was created!')
-                }
-            )
+
+            if (!found_ExportDefaultDeclaration) {
+                // throw path.buildCodeFrameError('ExportDefaultDeclaration not found')
+            }
+            if (!found_ClassDeclaration) {
+                // throw path.buildCodeFrameError('ClassDeclaration not found')
+            }
+            logger.debug('Contract ' + state.opts.basename + ' code and pfe transpiled')
         }
     }
 }
