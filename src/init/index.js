a42bca15fbe6fd872eba01d3acb16c7f83c0fb69
Alexey Bokarev <alexeybokarev@gmail.com>
2018-12-07 15:30:55 +0800

    fix init and replace in build command

--- src/init/index.js
+++ src/init/index.js
@@ -7,33 +7,48 @@ export default () => {
     // TODO must be executed in an empty folder
 
     const contracts = path.resolve(currentDir, './contracts')
+
     fs.stat(contracts, (err, stats) => {
         if (err) {
-            fs.mkdir(contracts)
+            fs.mkdir(contracts, function (err) {
+                //  console.log(err)
+            })
         }
     })
 
     const migrations = path.resolve(currentDir, './migrations')
     fs.stat(migrations, (err, stats) => {
         if (err) {
-            fs.mkdir(migrations)
+            fs.mkdir(migrations, function (err) {
+                //  console.log(err)
+            })
         }
     })
 
     const networks = path.resolve(currentDir, './network')
     fs.stat(networks, (err, stats) => {
         if (err) {
-            fs.mkdir(networks)
+            fs.mkdir(networks, function (err) {
+                // console.log(err)
+            })
         }
     })
 
     const test = path.resolve(currentDir, './test')
     fs.stat(test, (err, stats) => {
         if (err) {
-            fs.mkdir(test)
+            fs.mkdir(test, function (err) {
+                console.log(err)
+            })
         }
     })
 
     const config = path.resolve(currentDir, './giant.js')
-    fs.writeFile(config, 'module.exports = {}')
+    fs.writeFile(config, 'module.exports = {}', function (err) {
+        if (err) {
+            // console.log(err);
+        }
+    })
+
+    console.log('folders ./contracts ./migrations ./network ./test and config ./giant.js created')
 }
